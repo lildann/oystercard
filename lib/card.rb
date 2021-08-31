60ed08e02ca967e2ac12fd156664387678b8062d
EMDevelop <edwardmartin.inbox@gmail.com>
2021-08-31 16:40:44 +0100

    Oyster cards are now initialised with a balance of 0

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,9 @@
 class Oystercard
+
+  def initialize(balance=0)
+    @balance = balance
+    puts "hello Lilly"
+  end
+
+  attr_reader :balance
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,18 @@
 require 'card'
 describe Oystercard do 
+
+  context 'I want money on my card' do
+
+    it 'checks that a new oystercard is created with a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+
+  end
+
+  context 'I want to add money to my card' do
+
+    
+
+  end
+
 end
