42a8b21760225d346ed1228d790c3b178f219d64
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-01 15:34:11 +0100

    Updated descriptions

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,33 +1,26 @@
 require 'card'
-describe Oystercard do 
+describe Oystercard do
 
-  context 'I want money on my card' do
+  context 'when putting money on my card' do
 
-    it 'checks that a new oystercard is created with a balance of 0' do
+    it 'starts with a balance of 0' do
       expect(subject.balance).to eq 0
     end
 
-  end
-
-  context 'I want to add money to my card' do
-
     it 'adds money to balance' do 
-      subject.top_up(10)
       expect(subject).to respond_to(:top_up).with(1).argument
+      subject.top_up(10)
       expect(subject.balance).to eq(10)
     end
 
-  end
-
-  context "I don't want to put too much money on my card" do
-    
     it 'raises an error when the top up amount exceeds the £90 limit' do
       expect(described_class::BALANCE_LIMIT).to eq 90
       expect { subject.top_up(91) }.to raise_error "Top up amount exceeds £#{described_class::BALANCE_LIMIT}"
     end
+
   end
 
-  context "I need my fare deducated from my card" do
+  context "when deducting money from my card" do
 
     it 'deducts money from my balance' do
       expect(subject).to respond_to(:deduct).with(1).argument
